0f185847957aa285384486e34fdf7e14659bab15
igorAlves321 <igoralvespalmeirag4@gmail.com>
2024-11-03 01:12:48 -0300

    agora refatoramos o código tirando a repetições de funções que estavam nas classes diferentes, pois faziam que o código em geral ficasse muito repetitivo.

--- ScreenSound/db/ScreenSoundContext.cs
+++ ScreenSound/db/ScreenSoundContext.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ScreenSound.Modelos;
 
-namespace ScreenSound.db;
-
-public class ScreenSoundContext : DbContext
+namespace ScreenSound.Banco
 {
-    // Define a tabela Artistas
-    public DbSet<Artista> Artistas { get; set; }
+    public class ScreenSoundContext : DbContext
+    {
+        public DbSet<Artista> Artistas { get; set; }
         public DbSet<Musica> Musicas { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        // Define a string de conexão para o MySQL
-        optionsBuilder.UseMySql("server=localhost;database=ScreenSound;user=root;password=;port=3306",
-            new MySqlServerVersion(new Version(8, 0, 26))); // Ajuste a versão conforme necessário
+        public ScreenSoundContext(DbContextOptions<ScreenSoundContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;database=ScreenSound;user=root;password=;port=3306", 
+                new MySqlServerVersion(new Version(8, 0, 26)));
+        }
     }
 }
