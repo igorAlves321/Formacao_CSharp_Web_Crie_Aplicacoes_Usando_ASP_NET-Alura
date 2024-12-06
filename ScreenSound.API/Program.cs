54c66644a420fe8b14a40b107f1757faebe3e4d1
igorAlves <igoralvespalmeirag4@gmail.com>
2024-12-05 22:31:51 -0300

    correções de bugs feitas na api e na aplicação web. Finalizando curso. Agora iniciando o de autenticação e autorização.

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -5,46 +5,54 @@ using ScreenSound.API.Endpoints;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 using ScreenSound.Shared.Modelos.Modelos;
-using System.Data.SqlTypes;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ScreenSoundContext>((options) => {
+// Configuração do banco MySQL
+builder.Services.AddDbContext<ScreenSoundContext>((options) =>
+{
     options
-            .UseSqlServer(builder.Configuration["ConnectionStrings:ScreenSoundDB"])
-            .UseLazyLoadingProxies();
+        .UseMySql(
+            builder.Configuration["ConnectionStrings:ScreenSoundDB"],
+            new MySqlServerVersion(new Version(8, 0, 29)))
+        .UseLazyLoadingProxies();
 });
+
+// Serviços de Repositório
 builder.Services.AddTransient<DAL<Artista>>();
 builder.Services.AddTransient<DAL<Musica>>();
 builder.Services.AddTransient<DAL<Genero>>();
 
+// Configuração de Serialização
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(
+    options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// Configuração de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:7015") // URL do frontend
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+// Configuração de Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-
-builder.Services.AddCors(
-    options => options.AddPolicy(
-        "wasm",
-        policy => policy.WithOrigins([builder.Configuration["BackendUrl"] ?? "https://localhost:7089",
-            builder.Configuration["FrontendUrl"] ?? "https://localhost:7015"])
-            .AllowAnyMethod()
-            .SetIsOriginAllowed(pol => true)
-            .AllowAnyHeader()
-            .AllowCredentials()));
-
-
 var app = builder.Build();
 
-app.UseCors("wasm");
+// Ativar o CORS
+app.UseCors("AllowFrontend");
 
 app.UseStaticFiles();
-
 app.AddEndPointsArtistas();
 app.AddEndPointsMusicas();
 app.AddEndPointGeneros();
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
