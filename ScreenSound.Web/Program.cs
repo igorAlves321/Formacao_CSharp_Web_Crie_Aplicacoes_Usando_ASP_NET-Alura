4cd670eade309692474e23cdcbfc8feece72b9ea
igorAlves <igoralvespalmeirag4@gmail.com>
2024-12-09 23:55:27 -0300

    configuramos o HttpClient para interceptar a solicitação que está sendo enviada para o navegador e colocar o cookie. No arquivo Program.cs, criamos uma classe, que é responsável por fazer isso.

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -9,13 +9,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
-
+builder.Services.AddScoped<CookieHandler>();
 builder.Services.AddScoped<ArtistaAPI>();
 builder.Services.AddScoped<MusicaAPI>();
 
 builder.Services.AddHttpClient("API",client => {
     client.BaseAddress = new Uri(builder.Configuration["APIServer:Url"]!);
     client.DefaultRequestHeaders.Add("Accept", "application/json");
-});
+}).AddHttpMessageHandler<CookieHandler>();
 
 await builder.Build().RunAsync();
